ca9e718dabde5f0a5b1fe6580e75155933e602b7
Rajesh Kumar <54294184+rkumar-teaching@users.noreply.github.com>
2019-08-22 02:37:34 +0000

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# This line is just a comment, a comment line starts with a pund sign and generally intended to guide the program readers
+# This program is intended to print a bunch of messages/sentences
+print('Welcome to the world of computing!')
+print('If you wish to stay in this worl, Python would be your savior!')
+print('Read more about Python at "https://python.swaroopch.com/about_python.html"')
+# Your task is to add as many print statements as you want and introduce yourself 
+# in the most exciting fashion you can think of and then push the modified code 
